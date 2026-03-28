5b9cc082a5301713dc9b94b451c721ae6ab648ca
TundraClimate <tundra.tundere@gmail.com>
2026-03-28 12:14:03 +0900

    📝docs: Add docs for Artwork.cs

--- src/Artwork.cs
+++ src/Artwork.cs
@@ -2,8 +2,33 @@ using UnityEngine;
 
 namespace DeviceOfHermes.Resource;
 
+/// <summary>An additional artwork loader</summary>
+/// <remarks>
+/// The additional artworks that loaded by <see cref="Artwork"/> will clear when main initializer.<br/>
+/// therefore needs the lazy load and on-initialize hook.
+/// </remarks>
+/// <example><code>
+/// var unitBufPath = Path.Combine(typeof(MyModInitializer).GetAsmDirectory(), "Artwork", "BattleUnitBuf");
+///
+/// TextModel.OnLoadLocalize += lang => 
+/// {
+///     Artwork.LoadBattleUnitBufSprites(path, true);
+/// }
+/// </code></example>
 public static class Artwork
 {
+    /// <summary>Set <see cref="BattleUnitBuf"/> with id</summary>
+    /// <param name="unitBufId">Specific ID</param>
+    /// <param name="sprite">A sprite that shown</param>
+    /// <param name="replace">Is replace if contains same ID</param>
+    /// <remarks>
+    /// The unitBufId is correspond the <c>BattleUnitBuf.keywordIconId</c> by default<br/>
+    /// </remarks>
+    /// <example><code>
+    /// var unitBuf = Hermes.CreateSprite("MyUnitBuf.png");
+    ///
+    /// Artwork.SetBattleUnitBufSprite("MyUnitBuf", unitBuf);
+    /// </code></example>
     public static void SetBattleUnitBufSprite(string unitBufId, Sprite sprite, bool replace = false)
     {
         var iconDict = BattleUnitBuf._bufIconDictionary;
@@ -25,6 +50,15 @@ public static class Artwork
         iconDict.Add(unitBufId, sprite);
     }
 
+    /// <summary>Set <see cref="BattleUnitBuf"/> with path</summary>
+    /// <param name="imgPath">A unit texure path</param>
+    /// <param name="replace">Is replace if contains same ID</param>
+    /// <remarks>
+    /// An Id uses non-extension filename
+    /// </remarks>
+    /// <example><code>
+    /// Artwork.SetBattleUnitBufSprite("MyUnitBuf.png");
+    /// </code></example>
     public static void SetBattleUnitBufSprite(string imgPath, bool replace = false)
     {
         var imgFileName = Path.GetFileName(imgPath);
@@ -48,6 +82,16 @@ public static class Artwork
         SetBattleUnitBufSprite(imgId, imgPath, replace);
     }
 
+    /// <summary>Set <see cref="BattleUnitBuf"/> with path</summary>
+    /// <param name="unitBufId">Specific ID</param>
+    /// <param name="imgPath">A unit texure path</param>
+    /// <param name="replace">Is replace if contains same ID</param>
+    /// <remarks>
+    /// Overrides Id with <c>unitBufId</c>.
+    /// </remarks>
+    /// <example><code>
+    /// Artwork.SetBattleUnitBufSprite("UnitBuf", "MyUnitBuf.png");
+    /// </code></example>
     public static void SetBattleUnitBufSprite(string unitBufId, string imgPath, bool replace = false)
     {
         var sprite = Hermes.CreateSprite(imgPath);
@@ -62,6 +106,21 @@ public static class Artwork
         SetBattleUnitBufSprite(unitBufId, sprite, replace);
     }
 
+    /// <summary>Loads all images with under <c>rootDirPath</c></summary>
+    /// <param name="rootDirPath">A path that includes unitbuf sprite image</param>
+    /// <param name="replace">Is replace if contains same ID</param>
+    /// <remarks>
+    /// Method retrieves under path files on recursive.<br/>
+    /// Only read the png or jpeg extension file.
+    /// </remarks>
+    /// <example><code>
+    /// var unitBufPath = Path.Combine(typeof(MyModInitializer).GetAsmDirectory(), "Artwork", "BattleUnitBuf");
+    ///
+    /// TextModel.OnLoadLocalize += lang => 
+    /// {
+    ///     Artwork.LoadBattleUnitBufSprites(path, true);
+    /// }
+    /// </code></example>
     public static void LoadBattleUnitBufSprites(string rootDirPath, bool replace = false)
     {
         if (!Directory.Exists(rootDirPath))
