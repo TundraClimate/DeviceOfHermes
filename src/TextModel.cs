e5d455835b1a9627c682c845d9f4f02fff9cd56a
TundraClimate <tundra.tundere@gmail.com>
2026-03-28 14:00:05 +0900

    📝docs: Add docs for TextModel.cs

--- src/TextModel.cs
+++ src/TextModel.cs
@@ -4,6 +4,18 @@ using LOR_XML;
 
 namespace DeviceOfHermes.Resource;
 
+/// <summary>The localize helper</summary>
+/// <remarks>
+/// Set text data for directory.<br/>
+/// Additional data reset when change language, or restart game.<br/>
+/// Therefore, an event the <c>OnLoadLocalize</c> is hooks localize initialize.
+/// </remarks>
+/// <example><code>
+/// TextModel.OnLoadLocalize += lang =>
+/// {
+///     Hermes.Say($"Language changed to {lang}!");
+/// }
+/// </code></example>
 public static class TextModel
 {
     static TextModel()
@@ -16,8 +28,24 @@ public static class TextModel
         harmony.CreateClassProcessor(typeof(TextModelPatch.PatchOnetimeInvoke)).Patch();
     }
 
+    /// <summary>Invokes on localize data initialized</summary>
+    /// <remarks>Game start, language reset or etc.</remarks>
     public static event Action<string> OnLoadLocalize = lang => { };
 
+    /// <summary>Set BattleEffectText with text</summary>
+    /// <param name="text">A dataset of <see cref="BattleEffectText"/></param>
+    /// <param name="replace">Is replace if contains same text ID</param>
+    /// <remarks>
+    /// If <c>replace</c> is true, replaces same ID(ex. Burn) data.<br/>
+    /// </remarks>
+    /// <example><code>
+    /// TextModel.SetBattleEffectText(new BattleEffectText()
+    /// {
+    ///     ID = "Burn",
+    ///     Name = "Burn",
+    ///     Desc = "Will die",
+    /// }, true);
+    /// </code></example>
     public static void SetBattleEffectText(BattleEffectText text, bool replace = false)
     {
         ref var dict = ref EffectTextDict;
@@ -39,6 +67,28 @@ public static class TextModel
         dict.Add(text.ID, text);
     }
 
+    /// <summary>Set BattleEffectText with texts</summary>
+    /// <param name="texts">The datasets of <see cref="BattleEffectText"/></param>
+    /// <param name="replace">Is replace if contains same text ID</param>
+    /// <remarks>
+    /// If <c>replace</c> is true, replaces same ID(ex. Burn) data.<br/>
+    /// </remarks>
+    /// <example><code>
+    /// TextModel.SetBattleEffectTexts([
+    ///     new BattleEffectText()
+    ///     {
+    ///         ID = "Burn",
+    ///         Name = "Burn",
+    ///         Desc = "Will die",
+    ///     },
+    ///     new BattleEffectText()
+    ///     {
+    ///         ID = "Bleeding",
+    ///         Name = "Bleeding",
+    ///         Desc = "Will burst",
+    ///     }
+    /// ], true);
+    /// </code></example>
     public static void SetBattleEffectTexts(IEnumerable<BattleEffectText> texts, bool replace = false)
     {
         foreach (var text in texts)
