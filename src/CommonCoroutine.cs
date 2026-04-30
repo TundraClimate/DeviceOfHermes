f27d38bef9442c0c0980c0cc5b9b6c4d04b3f633
TundraClimate <tundra.tundere@gmail.com>
2026-05-01 03:40:16 +0900

    ✨feat: Add Fadein and Cgs Coroutine

--- src/CommonCoroutine.cs
+++ src/CommonCoroutine.cs
@@ -7,6 +7,79 @@ namespace DeviceOfHermes.UI;
 /// <summary>A list of Coroutines</summary>
 public class CommonCoroutine
 {
+    /// <summary>Fadein CanvasGroup</summary>
+    public static IEnumerator CanvasGroupFadein(CanvasGroup cg, float duration)
+    {
+        var elapsed = 0f;
+
+        cg.alpha = 0;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            cg.alpha = Mathf.SmoothStep(0f, 1f, elapsed / duration);
+
+            yield return null;
+        }
+
+        cg.alpha = 1;
+    }
+
+    /// <summary>Fadeout CanvasGroup</summary>
+    public static IEnumerator CanvasGroupFadeout(CanvasGroup cg, float wait, float duration)
+    {
+        var elapsed = 0f;
+
+        yield return new WaitForSeconds(wait);
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            cg.alpha = 1f - Mathf.SmoothStep(0f, 1f, elapsed / duration);
+
+            yield return null;
+        }
+
+        cg.alpha = 0;
+    }
+
+    /// <summary>Fadeout CanvasGroup and destroy</summary>
+    public static IEnumerator CanvasGroupFadeoutAndDestroy(CanvasGroup cg, float wait, float duration)
+    {
+        yield return CanvasGroupFadeout(cg, wait, duration);
+
+        var go = cg.gameObject;
+
+        UnityEngine.Object.Destroy(go);
+    }
+
+    /// <summary>Fadein Image</summary>
+    public static IEnumerator ImageFadein(Image image, float duration)
+    {
+        var elapsed = 0f;
+        var color = image.color;
+
+        color.a = 0;
+
+        image.color = color;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            color.a = Mathf.SmoothStep(0f, 1f, elapsed / duration);
+
+            image.color = color;
+
+            yield return null;
+        }
+
+        color.a = 1;
+        image.color = color;
+    }
+
     /// <summary>Fadeout Image</summary>
     public static IEnumerator ImageFadeout(Image image, float wait, float duration)
     {
