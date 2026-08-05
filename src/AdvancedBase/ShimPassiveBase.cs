95f0abefae072d5aaccc08b65e3e5ab13b1f4d9f
TundraClimate <tundra.tundere@gmail.com>
2026-08-05 17:59:07 +0900

    ✨feat: Improve ShimPassiveBase

--- src/AdvancedBase/ShimPassiveBase.cs
+++ src/AdvancedBase/ShimPassiveBase.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace DeviceOfHermes.AdvancedBase;
 
 /// <summary>Shimmering passive base</summary>
@@ -27,7 +29,13 @@ public abstract class ShimPassiveBase : AdvancedPassiveBase
     {
         var lid = vannila ? new LorId(id) : new LorId(PackageId, id);
 
-        var card = this.owner.allyCardDetail.AddTempCard(lid);
+        AddCard(lid);
+    }
+
+    /// <summary>Adds temp card to hand</summary>
+    public void AddCard(LorId id)
+    {
+        var card = this.owner.allyCardDetail.AddTempCard(id);
 
         card?.SetCostToZero();
     }
@@ -48,9 +56,12 @@ public abstract class ShimPassiveBase : AdvancedPassiveBase
     {
         Shimmering();
 
-        ApplyPattern();
+        if (!base.owner.IsBreakLifeZero())
+        {
+            ApplyPattern();
 
-        ResolvePriorities();
+            ResolvePriorities();
+        }
 
         Elapsed += 1;
     }
@@ -65,5 +76,103 @@ public abstract class ShimPassiveBase : AdvancedPassiveBase
     public string PackageId { private get; set; } = "";
 
     /// <summary>A turn of elapsed</summary>
-    public int Elapsed { get; set; }
+    public int Elapsed { get; private set; }
+
+    /// <summary>A list of patterns</summary>
+    protected virtual PatternList Patterns { get; } = new();
+
+    /// <summary>A list of pattern</summary>
+    protected class PatternList(bool loop = false) : IEnumerable<PatternInfo>
+    {
+        /// <summary>Applies pattern and next</summary>
+        public void ApplyNextPattern(ShimPassiveBase self)
+        {
+            if (_value.Count == 0)
+            {
+                return;
+            }
+
+            _index += 1;
+
+            if (_index >= 0 && _value.Count > _index)
+            {
+                _value[_index].ApplyPattern(self);
+            }
+            else if (_loop)
+            {
+                _index = -1;
+
+                ApplyNextPattern(self);
+            }
+        }
+
+        /// <summary>Adds element</summary>
+        public void Add(PatternInfo element)
+        {
+            _value.Add(element);
+        }
+
+        /// <summary>Impls GetEnumerator</summary>
+        public IEnumerator<PatternInfo> GetEnumerator() => _value.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _value.GetEnumerator();
+
+        private List<PatternInfo> _value = new();
+
+        private int _index = -1;
+
+        private bool _loop = loop;
+    }
+
+    /// <summary>A information of pattern</summary>
+    protected class PatternInfo(params List<object> ids)
+    {
+        internal void ApplyPattern(ShimPassiveBase self)
+        {
+            var pid = self.PackageId;
+            var ids = _ids.Filter(i => i is int or LorId);
+
+            foreach (var id in ids)
+            {
+                if (id is int num)
+                {
+                    self.AddCard(num);
+                }
+                else if (id is LorId lid)
+                {
+                    self.AddCard(lid);
+                }
+            }
+
+            var count = ids.Count();
+            var max = self.owner.Book.GetSpeedDiceRule(self.owner).Roll(self.owner).Count;
+
+            for (var i = count; max > i; i++)
+            {
+                filler?.Let(f => self.AddCard(f.fn(i)));
+            }
+        }
+
+        /// <summary>A filler</summary>
+        public Filler? filler = null;
+
+        private List<object> _ids = ids;
+    }
+
+    /// <summary>A filler</summary>
+    protected class Filler(Func<int, LorId> fn)
+    {
+        /// <summary>All uses id</summary>
+        public static Filler All(LorId id) => new(_ => id);
+
+        /// <summary>Constatnt uses id</summary>
+        public static Filler Const(params List<LorId> ids)
+        {
+            var q = new Queue<LorId>(ids);
+
+            return new(_ => q.Dequeue());
+        }
+
+        internal Func<int, LorId> fn = fn;
+    }
 }
