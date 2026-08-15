36d059eeb8b8ad7190d8979302bd0ca234437d7b
TundraClimate <tundra.tundere@gmail.com>
2026-08-15 20:16:01 +0900

    ✨feat: Add Limbuf buf icons

--- LimbufOfHermes/src/LimbufBootStrap.cs
+++ LimbufOfHermes/src/LimbufBootStrap.cs
@@ -1,4 +1,5 @@
 using DeviceOfHermes.Boot;
+using DeviceOfHermes.Resource;
 
 namespace LimbufOfHermes.Boot;
 
@@ -6,5 +7,47 @@ internal class LimbufBootStrap : HermesInitializer
 {
     public void OnInitMod()
     {
+        LoadAllBufIcons();
     }
+
+    static void LoadAllBufIcons()
+    {
+        foreach (var (name, data) in _manifest)
+        {
+            if (!name.EndsWith(".png"))
+            {
+                continue;
+            }
+
+            var id = name.Split('.')[2];
+
+            Artwork.SetBattleUnitBufSprite(id, Artwork.CreateSprite(data)!);
+        }
+    }
+
+    static Dictionary<string, byte[]> InitEmbeds()
+    {
+        var assembly = typeof(LimbufBootStrap).Assembly;
+        var result = new Dictionary<string, byte[]>();
+
+        foreach (var resourceName in assembly.GetManifestResourceNames())
+        {
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+
+            if (stream == null)
+            {
+                continue;
+            }
+
+            using var ms = new MemoryStream();
+
+            stream.CopyTo(ms);
+
+            result[resourceName] = ms.ToArray();
+        }
+
+        return result;
+    }
+
+    private static Dictionary<string, byte[]> _manifest = InitEmbeds();
 }
